dbf5692f6e784e6a0d1589057d833c14c7d0cccb
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-10 18:44:40 +0000

    Added data.txt.

--- pII/CSC207Project/src/backend/Flight.java
+++ pII/CSC207Project/src/backend/Flight.java
@@ -1,5 +1,48 @@
 package backend;
 
+import java.sql.Time;
+
 public class Flight {
+	private String origin; 
+	private String destination; 
+	private float cost;
+	private Time travelTime; 
+	private String airline; 
+	private int flightNumber;
+	
+	public Flight(String origin, String destination, float cost, Time travelTime, String airline, int flightNumber) {
+		super();
+		this.origin = origin;
+		this.destination = destination;
+		this.cost = cost;
+		this.travelTime = travelTime;
+		this.airline = airline;
+		this.flightNumber = flightNumber;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	public Time getTravelTime() {
+		return travelTime;
+	}
+
+	public String getAirline() {
+		return airline;
+	}
+
+	public int getFlightNumber() {
+		return flightNumber;
+	} 
+	
 
 }
