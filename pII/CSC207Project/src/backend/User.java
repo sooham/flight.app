47e11eb1ea5d89dbe8cba7e0844588c233f0a5b0
g5ccanni <g5ccanni@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-12 22:31:27 +0000

    Updated User

--- pII/CSC207Project/src/backend/User.java
+++ pII/CSC207Project/src/backend/User.java
@@ -2,6 +2,7 @@ package backend;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import backend.Flight;
 
 public abstract class User implements Serializable{
@@ -14,6 +15,28 @@ public abstract class User implements Serializable{
 		this.password = password;
 	}
 	
+	public void logIn(String test_username, String test_password){
+		
+	}
 	
+	public ArrayList<Flight> viewFlights(String origin, String departure, 
+			Calendar departureDate){
+		// uses a method from FlightManager
+	}
+	
+	public ArrayList<Iternary> viewItineraries(String origin, String departure, 
+			Calendar departureDate){
+		// uses a method from IternaryManager
+	}
+	
+	public ArrayList<Flight> sortFlights(String origin, String departure, 
+			Calendar departureDate){
+		// uses a method from FlightManager
+	}
+	
+	public ArrayList<Iternary> sortItineraries(String origin, String departure, 
+			Calendar departureDate){
+		// uses a method from IternaryManager
+	}
 
 }
