4c6c3b607a8f5a078668ed7eca3338ce0e73602a
c4limamb <c4limamb@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-15 04:19:37 +0000

    -Added partial implementation of Admin (methods mostly empty, to implement with teammates)

--- pII/CSC207Project/src/backend/Admin.java
+++ pII/CSC207Project/src/backend/Admin.java
@@ -1,5 +1,34 @@
 package backend;
 
-public class Admin {
+import java.io.Serializable;
 
+public class Admin extends User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2379924511936602176L;
+	String username;
+	String password;
+	
+	public Admin(String username, String password) {
+		super(username, password);
+	}
+	
+	public void editUserInfo() {
+		
+	}
+	
+	public String viewClientInfo() {
+		return "Insert client info code here.";
+	}
+	
+	public void addFlight(Flight toAdd) {
+		
+	}
+	
+	public void addFlightFromFile() {
+		
+	}
+	
 }
