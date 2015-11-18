77bf5827bcdfe9a7247334d17ce5c4de416b8ccf
Sooham Rafiz <rafizsooham@gmail.com>
2015-11-18 01:22:04 +0000

    Better javadocs for ClientManager

--- pII/CSC207Project/src/backend/ClientManager.java
+++ pII/CSC207Project/src/backend/ClientManager.java
@@ -1,20 +1,21 @@
 package backend;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.Serializable;
 
 /**
- * @author Christopher Cannistraro
- *
+ * A ClientManager object. A ClientManager is a singleton object used to
+ * handle Client (Not Implemented), in phase II the ClientManager works with
+ * the User class.
+ * 
+ * <p>This class provides methods to search and view stored client information.
  */
 public class ClientManager implements Serializable{
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private ArrayList<User> clients;
+	private static final long serialVersionUID = 1772117296747939656L;
+
+	public static ArrayList<User> clients; // The ArrayList of Clients
 	
 	/**
 	 * Creates a new ClientManager object
@@ -24,47 +25,47 @@ public class ClientManager implements Serializable{
 	}
 	
 	/**
-	 * Adds a client to the ClientManager 
-	 * @param c,  a client that is added into ClientManager
+	 * Adds a User to this ClientManager 
+	 * 
+	 * @param u  User that is to be added
 	 */
-	public void addClient(User c){
-		this.clients.add(c);
+	public void addClient(User u){
+		clients.add(u);
 	}
 	
 	/**
-	 * Returns a ArrayList of Clients based on a searchItem 
-	 * that is of a certain type
-	 * @param searchItem, a string with the item to search for
-	 * @param type, the type of object that the string is representing
-	 * @return specificClients, an ArrayList of Clients that fits the 
-	 * search item
+	 * Returns a ArrayList of Users based on a search of a key
+	 * that is of a certain value from this ClientManager.
+	 * 
+	 * @param key  the string name for the item to search for.
+	 * @param value  the value of key being searched for.
+	 * @return an ArrayList of User  that meet the search criterion
 	 */
-	public ArrayList<User> getClient(String searchItem, String type){
+	public ArrayList<User> getClient(String key, String value){
 		ArrayList<User> specificClients = new ArrayList<User>();
-		for (User c: this.clients){
-			if (type == "last name"){
-				String lastname = c.getLastName();
-				if (lastname == searchItem){
-					specificClients.add(c);
+		for (User u: clients){
+			if (key == "last name"){
+				String lastname = u.getLastName();
+				if (lastname == value){
+					specificClients.add(u);
 				}
 			}
-			else if (type == "first name"){
-				String firstname = c.getFirstName();
-				if (firstname == searchItem){
-					specificClients.add(c);
+			else if (key == "first name"){
+				String firstname = u.getFirstName();
+				if (firstname == value){
+					specificClients.add(u);
 				}
 			}
-            else if (type == "email"){
-            	String email = c.getEmail();
-				if (email == searchItem){
-					specificClients.add(c);
+            else if (key == "email"){
+            	String email = u.getEmail();
+				if (email == value){
+					specificClients.add(u);
 				}
-				
 			}
-            else if (type == "address"){
-            	String address = c.getAddress();
-				if (address == searchItem){
-					specificClients.add(c);
+            else if (key == "address"){
+            	String address = u.getAddress();
+				if (address == value){
+					specificClients.add(u);
 				}
 			}
 		}
@@ -72,56 +73,63 @@ public class ClientManager implements Serializable{
 	}
 	
 	/**
-	 * Returns a ArrayList of Clients based on the CreditCardNumber
-	 * and ExpiryDate
-	 * @param CreditCardNumber, the credit card number to search for
-	 * @param ExpiryDate, the expiration date to search for
-	 * @return specificClients, an ArrayList of Clients that fits the 
+	 * Returns a ArrayList of User based on the CreditCardNumber
+	 * and ExpiryDate from this ClientManager.
+	 * 
+	 * @param CreditCardNumber  the credit card number to search for
+	 * @param ExpiryDate  the expiration date to search for
+	 * @return specificClients   an ArrayList of User that fits the 
 	 * CreditCardNumber and ExpiryDate
 	 */
 	public ArrayList<User> getClient(int CreditCardNumber,
 			Date ExpiryDate){
 		ArrayList<User> specificClients = new ArrayList<User>();
-		for (User c: this.clients){
-			int creditcardnum = c.getCreditCardNumber();
-			Date expdate = c.getExpiryDate();
-			if (creditcardnum == CreditCardNumber && expdate == ExpiryDate){
-				specificClients.add(c);
+		for (User u: clients){
+			int creditCardNum = u.getCreditCardNumber();
+			Date expDate = u.getExpiryDate();
+			if (creditCardNum == CreditCardNumber && expDate == ExpiryDate){
+				specificClients.add(u);
 			}
 		}
 		return specificClients;
 	}
 	
 	/**
-	 * Returns the index of the user in the the client manager. If the client is not there 
-	 * returns -1. 
-	 * @param lastName a string of the last name of the user.
-	 * @param firstName a string of the first name of the user.
-	 * @param email a string of the unique email address for the user. 
-	 * @param address a string of the billing address for the user.
-	 * @param creditCardNum a credit card number. 
-	 * @param expiryDate the expiry date of the credit card number. 
-	 * @return returns index representing the client in the clientManager. if the client is 
-	 * not there returns -1
+	 * Returns the index of the User with given fields in this
+	 * client manager. If the User is not there returns -1. 
+	 * 
+	 * @param lastName  a String of the last name of the user.
+	 * @param firstName  a String of the first name of the user.
+	 * @param email  a String of the unique email address for the user. 
+	 * @param address  a String of the billing address for the user.
+	 * @param creditCardNum  a credit card number. 
+	 * @param expiryDate  the expiry-date of the credit card number. 
+	 * @return the index representing the user in this ClientManager. If User
+	 * does not exist return -1. 
 	 */
-	public int getClientIndex(String lastName, String firstName, String email, String address,
-			int creditCardNum, String expiryDate){
-		User client = new User(lastName, firstName, email, address, creditCardNum, expiryDate);
-		return getClientIndex(client);	
+	public int getClientIndex(String lastName, String firstName, String email,
+			String address, int creditCardNum, String expiryDate){
+		User user = new User(
+				lastName, firstName, email, address, creditCardNum, expiryDate
+			);
+		return getClientIndex(user);	
 	}
 	
 	/**
-	 * Returns the index of the user in the the client manager. If the client is not there 
-	 * returns -1. 
-	 * @param client a client object. 
-	 * @return returns index representing the client in the clientManager. if the client is 
-	 * not there returns -1
+	 * Returns the index of the User in this ClientManager. If the client is
+	 * not there returns -1. 
+	 * 
+	 * @param user  a User object. 
+	 * @return returns index representing the User in this ClientManager.
+	 * If the User does not exist returns -1.
 	 */
-	public int getClientIndex(User client){
-		return this.clients.indexOf(client);
+	public int getClientIndex(User user){
+		return clients.indexOf(user);
 	}
 
 	/**
+	 * Returns the ArrayList of User from this ClientManager.
+	 * 
 	 * @return the clients
 	 */
 	public ArrayList<User> getClients() {
