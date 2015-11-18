8e3dd555b0107ea48ce820d2e97a30a0330bdd10
c4rafizs <c4rafizs@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-18 01:23:16 +0000

    Removed uneeded imports from Itinerary

--- pII/CSC207Project/src/backend/Itinerary.java
+++ pII/CSC207Project/src/backend/Itinerary.java
@@ -1,9 +1,5 @@
 package backend;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.TreeSet;
