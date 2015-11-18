17604d31362d780b350a36143d2e7313a6a20775
c4rafizs <c4rafizs@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-18 01:41:53 +0000

    Updated FileDatabase with better style and docs

--- pII/CSC207Project/src/backend/FileDatabase.java
+++ pII/CSC207Project/src/backend/FileDatabase.java
@@ -2,58 +2,58 @@
 package backend;
 
 import java.io.BufferedReader;
+import java.util.Date;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.text.ParseException;
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
- * The main class of the program. Will store all clients, flights and itineraries in static 
- * variables. Clients will be stored in a client manager, while the flights and itineraries 
- * will be stored in a flight manager. This class will also deal with taking input from csv
- * files.  
+ * The FileDataBase class. This class stores all User, Flight and Itinerary 
+ * objects for the app. Clients will be stored in a ClientManager, while the
+ * Flights and Itinerary will be stored in a FlightManager.
+ * 
+ * <p>This class has method for reading CSV files, updating the backend
+ * and Serializing runime status of the app into persistent .ser files.
  */
+
 public class FileDatabase implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -5414755056678568378L;
 	private static ClientManager clientManager = new ClientManager();
-	private static FlightManager flightManager = new FlightManager();
-	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	private static FlightManager flightManager = FlightManager.getInstance();
+	private static SimpleDateFormat format = new SimpleDateFormat(
+												 	"yyyy-MM-dd HH:mm");
 
 	/**
 	 * Imports the files from the given path. If the files are not found will 
 	 * create new blank Manager class in the given directory. 
+	 * 
+	 * @param dir  the path of the file
 	 */
 	public FileDatabase(String dir) {
 		importFiles(dir);
 	}
 
 	/**
-	 * This will contain the loop of the final program
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
 	 * Takes the location of a directory and saves the ClientManger and
 	 * ItineraryManger objects.
-	 * @param dir the path of a directory in the system.
+	 * 
+	 * @param dir  the path of a directory in the system.
 	 */
 	public static void migrateFiles(String dir) {
 		try {
-			FileOutputStream fileout = new FileOutputStream(dir + "/ClientManager.ser");
-			FileOutputStream fileout2 = new FileOutputStream(dir + "/FlightManager.ser");
+			FileOutputStream fileout = new FileOutputStream(
+					dir + "/ClientManager.ser"
+			);
+			FileOutputStream fileout2 = new FileOutputStream(
+					dir + "/FlightManager.ser"
+			);
 			ObjectOutputStream out = new ObjectOutputStream(fileout);
 			ObjectOutputStream out2 = new ObjectOutputStream(fileout2);
 			out.writeObject(clientManager);
@@ -68,6 +68,8 @@ public class FileDatabase implements Serializable {
 	}
 	
 	/**
+	 * Returns the ClientManager for this FileDatabase.
+	 * 
 	 * @return the clientManger
 	 */
 	public static ClientManager getClients() {
@@ -75,7 +77,9 @@ public class FileDatabase implements Serializable {
 	}
 
 	/**
-	 * @return the itineraryManger
+	 * Returns the FlightManager for this FileDatabase.
+	 * 
+	 * @return the flightManager
 	 */
 	public static FlightManager getFlightManger() {
 		return flightManager;
@@ -83,20 +87,25 @@ public class FileDatabase implements Serializable {
 	
 	/**
 	 * Imports the manager classes from a specific directory passed. 
-	 * @param dir the path where the files are located. 
+	 * 
+	 * @param dir  the path where the files are located. 
 	 */
 	public static void importFiles(String dir){
 		try {
-			FileInputStream filein = new FileInputStream(dir +"/ClientManager.ser");
-			FileInputStream filein2 = new FileInputStream(dir +"/FlightManager.ser");
+			FileInputStream filein = new FileInputStream(
+					dir +"/ClientManager.ser"
+					);
+			FileInputStream filein2 = new FileInputStream(
+					dir +"/FlightManager.ser"
+					);
 			ObjectInputStream in = new ObjectInputStream(filein);
 			ObjectInputStream in2 = new ObjectInputStream(filein2);
 			clientManager = (ClientManager) in.readObject();
 			flightManager = (FlightManager) in.readObject();
 			filein.close();
-		}catch (IOException i){
+		} catch (IOException i){
 			migrateFiles(dir);
-		}catch(ClassNotFoundException c){
+		} catch(ClassNotFoundException c){
 			c.printStackTrace();
 			return;
 		}
@@ -105,7 +114,8 @@ public class FileDatabase implements Serializable {
 	/**
 	 * Adds a user from a given file. Takes a directory including the file name. 
 	 * File must be a csv file. Can be txt but must be separated with commas.  
-	 * @param dir a string of the path to the csv file. 
+	 * 
+	 * @param dir  a string of the path to the csv file. 
 	 */
 	public static void getInfoFromFile(String dir){
 		FileReader in = null;
@@ -122,15 +132,17 @@ public class FileDatabase implements Serializable {
 		} catch (IOException e) {
 			System.out.println("The file was not found.");
 		} catch(IndexOutOfBoundsException e){
-			System.out.println("The file does not have enough arguements to make a client.");
+			System.out.println(
+				"The file does not have enough arguements to make a client."
+			);
 		}
-		
 	}
 	
 	/**
 	 * Adds a flight from a given file. Takes a directory including the file name. 
 	 * File must be a csv file. Can be txt but must be separated with commas.  
-	 * @param dir a string of the path to the csv file. 
+	 * 
+	 * @param dir  a string of the path to the csv file. 
 	 * @throws InvalidFlightException 
 	 * @throws NumberFormatException 
 	 */
@@ -147,13 +159,16 @@ public class FileDatabase implements Serializable {
 			Date departureTime = format.parse(values[3]);
 			Date arrivalTime = format.parse(values[4]);
 			//Creates a new flight object. 
-			Flight newFlight = new Flight(values[5], Long.parseLong(values[6]), values[0],
-						values[1], arrivalTime, departureTime,Double.parseDouble(values[2]));
+			Flight newFlight = new Flight(values[5], Long.parseLong(values[6]),
+					values[0], values[1], arrivalTime, departureTime,
+					Double.parseDouble(values[2]));
 			flightManager.addFlight(newFlight);
 		}catch (IOException e) {
 			System.out.println("The file was not found.");
 		}catch (ParseException e) {
-		    System.out.println("The date is not in the right format: yyyy-MM-dd HH:mm");
+		    System.out.println(
+		    		"The date is not in the right format: yyyy-MM-dd HH:mm"
+		    		);
 		}catch (IndexOutOfBoundsException e) {
 			System.out.println("Missing flight data.");
 		}catch (InvalidFlightException e){
@@ -161,7 +176,5 @@ public class FileDatabase implements Serializable {
 		}catch (NumberFormatException e) {
 			System.out.println("The flight number is not in the right format.");	
 		}
-			
 	}
-
 }
