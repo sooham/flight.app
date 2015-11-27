5d0dd5250c247f36a3178ea5374f71e84fdb742a
c4limamb <c4limamb@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-27 00:13:27 +0000

    -Added editClientInfo, viewClientInfo methods to Admin class -Added viewInfo, editInfo to Client class -Made addFlightFromFile and addUserFromFile Admin only (they do nothing if used by Client class)

--- pII/CSC207Project/src/backend/Admin.java
+++ pII/CSC207Project/src/backend/Admin.java
@@ -15,7 +15,18 @@ public class Admin extends User implements Serializable {
 			int creditCardNumber, Date expiryDate, String password) {
 		super(LastName, firstName, email, address, creditCardNumber,
 				expiryDate, password);
+		
 	}
+	
+	/**
+	 * Allows the admin to view all of the client's information in the form
+	 * of a string. The information is seen the following way:
+	 * LastName, firstName, email, address, creditCardNumber, expiryDate
+	 * @param theClient The client that will have his information viewed
+	 * @return A string containing all client information.
+	 * @throws MissingUserException if the user in question does not exist.
+	 * @throws InvalidUserException if the user in question is an admin
+	 */
 	
 	public String viewClientInfo(User theClient) throws MissingUserException,
 	InvalidUserException {
@@ -23,8 +34,8 @@ public class Admin extends User implements Serializable {
 		if (theClient == null) {
 			throw new MissingUserException("This user does not exist.");
 		}
-		else if (theClient instanceof Admin) {
-			throw new InvalidUserException("Cannot view another admin's info.");
+		else if (theClient instanceof Admin || !(theClient instanceof Client)) {
+			throw new InvalidUserException("Invalid user.");
 		}
 		else {
 			result = 
@@ -38,6 +49,28 @@ public class Admin extends User implements Serializable {
 		return result;
 		
 		}
-
+	
+	/**
+	 * Allows the admin to edit a client's information by calling the client's own
+	 * information edit method
+	 * @param theClient The client with info to be editted
+	 * @param index The information to be editted
+	 * @param o The information to be placed
+	 * @throws InvalidUserException if User is an admin or not an actual user
+	 * @throws MissingUserException if User does not exist
+	 */
+	
+	public void editClientInfo(User theClient, int index, Object o) throws
+	InvalidUserException, MissingUserException {
+		if (theClient == null) {
+			throw new MissingUserException("User does not exist.");
+		}
+		else if (theClient instanceof Admin || !(theClient instanceof Client)) {
+			throw new InvalidUserException("Invalid user.");
+		}
+		else {
+			((Client) theClient).editInfo(index, o);
+		}
+	}
 }
 	
--- pII/CSC207Project/src/backend/Client.java
+++ pII/CSC207Project/src/backend/Client.java
@@ -17,13 +17,80 @@ public class Client extends User implements Serializable {
 				expiryDate, password);
 	}
 	
+	/**
+	 * This method is unavailable to clients and is meant to be Admin only.
+	 */
+	
 	@Override
-	public void addFlightFromFile(String dir) throws InvalidUserException{
-		throw new InvalidUserException("Access denied, User must be Admin to" +
-	" perform this action.");
+	public void addFlightFromFile(String dir) {}
+	
+	/**
+	 * This method is unavailable to clients and is meant to be Admin only.
+	 */
+	
+	@Override
+	public void addUserFromFile(String dir) {}
+	
+	/**
+	 * Allows the client to view all of his personal and billing information.
+	 * @return Personal and billing information (String)
+	 */
+	
+	public String viewInfo() {
+		return String.format(this.getLastName(), ", ",
+				this.getFirstName(), ", ",
+				this.getEmail(), ", ",
+				this.getAddress(), ", ",
+				String.valueOf(this.getCreditCardNumber()), ", ",
+				String.valueOf(this.getExpiryDate()));
 	}
 	
+	/**
+	 * Allows the client to edit his personal and billing info
+	 * @param index The information to edit, going from 0 to 6,
+	 * it follows the constructor order, last one being the password
+	 * @param o what will the information be set, must be of the same type
+	 */
 	
+	public void editInfo(int index, Object o) {
+		if (!(index > 6) && !(index < 0)) {
+			if (index == 0) {
+				if (o.getClass() == this.getLastName().getClass()) {
+					this.setLastName((String) o);
+				}
+			}
+			else if (index == 1) {
+				if (o.getClass() == this.getFirstName().getClass()) {
+					this.setFirstName((String) o);
+				}
+			}
+			else if (index == 2) {
+				if (o.getClass() == this.getEmail().getClass()) {
+					this.setEmail((String) o);
+				}
+			}
+			else if (index == 3) {
+				if (o.getClass() == this.getAddress().getClass()) {
+					this.setAddress((String) o);
+				}
+			}
+			else if (index == 4) {
+				if (o instanceof Integer) {
+					this.setCreditCardNumber((int) o);
+				}
+			}
+			else if (index == 5) {
+				if (o.getClass() == this.getExpiryDate().getClass()) {
+					this.setExpiryDate((Date) o);
+				}
+			}
+			else if (index == 6) {
+				if (o.getClass() == this.getPassword().getClass()) {
+					this.setPassword((String) o); //Add Password setter for User
+				}
+			}
+		}
+	}
 	
 
 }
