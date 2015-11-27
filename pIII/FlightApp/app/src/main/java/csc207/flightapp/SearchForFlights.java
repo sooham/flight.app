4c2ad79b1a71fa57876a977c13de7cfa1b3241ba
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-27 15:06:58 +0000

    Input is taken between activities.

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForFlights.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForFlights.java
@@ -1,7 +1,10 @@
 package csc207.flightapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SearchForFlights extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class SearchForFlights extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_for_flights);
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("EMAIL");
+        TextView textView = (TextView)findViewById(R.id.display_email);
+        textView.setText(email);
+
+
     }
 }
