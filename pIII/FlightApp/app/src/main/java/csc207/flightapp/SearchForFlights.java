082cea22a03c50bead4c5ce83a05f9f43c87469d
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-11-27 19:10:03 +0000

    Finished table creation for viewing Flights.

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForFlights.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForFlights.java
@@ -3,10 +3,15 @@ package csc207.flightapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-import backend.*;
+
 
 public class SearchForFlights extends AppCompatActivity {
+    EditText origin;
+    EditText destination;
+    EditText departureDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,22 @@ public class SearchForFlights extends AppCompatActivity {
         String email = intent.getStringExtra("EMAIL");
         TextView textView = (TextView)findViewById(R.id.display_email);
         textView.setText(email);
+    }
+
+    /**
+     * Will pass the user input to an activity that finds all the flights.
+     * @param view the current view of the app.
+     */
+    public void viewFlights(View view){
+        origin = (EditText)findViewById(R.id.origin);
+        destination = (EditText)findViewById(R.id.destination);
+        departureDate = (EditText)findViewById(R.id.departure_date);
+        Intent intent = new Intent(this, SearchForFlights.class);
+
+        //passes the input information to the view_view_searched_flights.
+        intent.putExtra("ORIGIN", origin.getText().toString());
+        intent.putExtra("DESTINATION", destination.getText().toString());
+        intent.putExtra("DEPARTURE_DATE", departureDate.getText().toString());
+        startActivity(intent);
     }
 }
