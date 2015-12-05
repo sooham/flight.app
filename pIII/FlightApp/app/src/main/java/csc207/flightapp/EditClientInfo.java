f32d4e42ff22156a25eca66b780b0bf771ca9251
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-12-05 21:53:19 +0000

    Saving changes to clients now work somewhat.

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/EditClientInfo.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/EditClientInfo.java
@@ -3,8 +3,15 @@ package csc207.flightapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import backend.FileDatabase;
 import backend.User;
@@ -12,6 +19,15 @@ import backend.User;
 public class EditClientInfo extends AppCompatActivity {
     User user =null;
     String email;
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat(
+            "yyyy-MM-dd");
+
+    //The views in the activity
+    EditText firstName;
+    EditText lastName;
+    EditText creditNum;
+    EditText expiryDate;
+    TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +39,44 @@ public class EditClientInfo extends AppCompatActivity {
         user = FileDatabase.getInstance().getUserManager().getUserWithEmail(email);
 
         //Sets the default values for all views
-        EditText firstName = (EditText)findViewById(R.id.first_name);
-        EditText lastName = (EditText)findViewById(R.id.last_name);
-        EditText creditNum = (EditText)findViewById(R.id.credit_card_num);
-        EditText expiryDate = (EditText)findViewById(R.id.expiry_date);
-        TextView username = (TextView) findViewById(R.id.username);
+        firstName = (EditText)findViewById(R.id.first_name);
+        lastName = (EditText)findViewById(R.id.last_name);
+        creditNum = (EditText)findViewById(R.id.credit_card_num);
+        expiryDate = (EditText)findViewById(R.id.expiry_date);
+        username = (TextView) findViewById(R.id.username);
+        try{
+            firstName.setText(user.getFirstName());
+            lastName.setText(user.getLastName());
+            creditNum.setText(user.getCreditCardNumber());
+            expiryDate.setText(user.getCreditCardNumber());
+        }catch(NullPointerException e){
+
+        }catch (RuntimeException e){
+
+        }
         username.setText(email);
+    }
+
+    public void saveChanges(View view){
+        user.setFirstName(firstName.getText().toString());
+        user.setLastName(lastName.getText().toString());
+        user.setCreditCardNumber(Integer.parseInt(creditNum.getText().toString()));
+        try{
+            Date newDate = dateFormatter.parse(expiryDate.getText().toString());
+            user.setExpiryDate(newDate);
+        }catch(ParseException e){
+
+        }
+
+        //Saves the changes made to the user and the UserManager
+        try{
+            FileDatabase.getInstance().serializeManagers(
+                    this.getApplicationContext().getFilesDir().getCanonicalPath()+"/");
+        }catch(IOException e){
+
+        }
+        Intent intent = new Intent(this, SearchForFlights.class);
+        intent.putExtra("EMAIL", email);
+        startActivity(intent);
     }
 }
