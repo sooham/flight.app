7707d08ff115138af7f36e67040408eae9f43a1b
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-12-06 18:43:13 +0000

    Added a button to book Flights

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/ViewSearchedFlights.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/ViewSearchedFlights.java
@@ -1,20 +1,27 @@
 package csc207.flightapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 import backend.*;
 
-public class ViewSearchedFlights extends AppCompatActivity {
+public class ViewSearchedFlights extends AppCompatActivity implements View.OnClickListener{
     private FlightManager FLIGHTS = null;
     private TableLayout table;
+    private Flight bookFlight;
+    private User user;
 
     /**
      * Creates the activity for viewing flights.
@@ -25,10 +32,16 @@ public class ViewSearchedFlights extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_searched_flights);
-        FileDatabase.getInstance().addFlightFromFile("/data/flights1.txt");
+        try {
+            FileDatabase.getInstance().addFlightFromFile(this.getApplicationContext().getFilesDir().getCanonicalPath() + "/flights1.txt");
+        }catch(IOException c){
+
+        }
         FLIGHTS = FileDatabase.getInstance().getFlightManger();
         table = (TableLayout)findViewById(R.id.flight_table);
         Intent intent = getIntent();
+        user = FileDatabase.getInstance().getUserManager().getUserWithEmail(
+                intent.getStringExtra("EMAIL"));
         List<Flight> flights = FLIGHTS.getFlights(intent.getStringExtra("ORIGIN"),
                 intent.getStringExtra("DESTINATION"),
                intent.getStringExtra("DEPARTURE_DATE"));
@@ -77,18 +90,42 @@ public class ViewSearchedFlights extends AppCompatActivity {
             flightText4.setPadding(5,5,5,5);
             tableRow.addView(flightText4);
 
-            // Creation of the textView that displays the num available seats.
+            /*Creation of the textView that displays the num available seats.
             TextView flightText5 = new TextView(this);
             flightText5.setText(Objects.toString(flight.getNumEmptySeats()));
             flightText5.setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
             flightText5.setPadding(5,5,5,5);
             tableRow.addView(flightText5);
+            */
+
+            //Adds a button to book the flight
+            MyButton button = new MyButton(this);
+            button.setText("Book");
+            button.setTextSize(8);
+            button.flight = flight;
+            button.callOnClick();
+            tableRow.addView(button, 300, 100);
 
             //adds the row to the table
             table.addView(tableRow, new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
         }
+    }
+
+    @Override
+    public void onClick(View view){
+        user.bookItinerary(((MyButton)view).flight);
+
+    }
+
+    //constructor for a custom button.
+    public class MyButton extends Button{
+        public Flight flight;
+        public MyButton(Context c){
+            super(c);
+        }
+
     }
 
 }
