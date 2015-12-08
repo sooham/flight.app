2a2a1621cce5486712bf81d5756fa205dc52955e
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-12-08 14:26:41 +0000

    Added Admin editing client info activity.

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForClients.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForClients.java
@@ -1,13 +1,40 @@
 package csc207.flightapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import backend.FileDatabase;
+import backend.UserManager;
 
 public class SearchForClients extends Activity {
+    UserManager userManager;
+    EditText clientEmail;
+    TextView loginFailure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_for_clients);
+        userManager = FileDatabase.getInstance().getUserManager();
+
+    }
+
+    public void editClientInfo(View view) {
+        clientEmail = (EditText) findViewById(R.id.get_client_email);
+        String email = clientEmail.getText().toString();
+        if(userManager.getUserWithEmail(email)!= null){
+            Intent intent = new Intent(this, EditClientInfo.class);
+            intent.putExtra(UserLogin.EMAIL, email);
+            startActivity(intent);
+        }else{
+            loginFailure = (TextView) findViewById(R.id.client_failure);
+            loginFailure.setText("Client email is incorrect.");
+        }
+
+
     }
 }
