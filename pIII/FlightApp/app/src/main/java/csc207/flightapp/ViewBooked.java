ab77a18e3d13d6194d1603e6809410c6e9c18a8b
c5singha <c5singha@2e5ea3a6-4f0b-40ad-9d3a-b63c58092c43>
2015-12-08 02:10:04 +0000

    Completed booking itineraries and viewing booked flights.

--- pIII/FlightApp/app/src/main/java/csc207/flightapp/ViewBooked.java
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/ViewBooked.java
@@ -1,66 +1,63 @@
 package csc207.flightapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import backend.*;
+import backend.FileDatabase;
+import backend.Flight;
+import backend.FlightManager;
+import backend.Itinerary;
+import backend.User;
 
-public class ViewSearchedFlights extends AppCompatActivity {
+public class ViewBooked extends Activity implements View.OnClickListener{;
     private TableLayout table;
+    private Flight bookFlight;
+    private User user;
 
-    /**
-     * Creates the activity for viewing flights.
-     *
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_searched_flights);
+        setContentView(R.layout.activity_view_booked);
         table = (TableLayout)findViewById(R.id.flight_table);
         Intent intent = getIntent();
-        createTable((ArrayList<Flight>)intent.getSerializableExtra("Flights"));
+        user = FileDatabase.getInstance().getUserManager().getUserWithEmail(
+                intent.getStringExtra("EMAIL"));
+        createTable(user.getBookedItineraries());
     }
 
     /**
      * Creates the table for displaying all the searched itineraries.
      *
-     * @param flights takes the key for what flights to be displayed.
+     * @param itineraries takes the key for what flights to be displayed.
      */
-    private void createTable(ArrayList<Flight> flights){
-        for (Flight flight:flights) {
+    private void createTable(ArrayList<Itinerary> itineraries){
+        for (Itinerary flight:itineraries) {
+            System.out.print(flight.getOrigin());
             TableRow tableRow = new TableRow(this);
             tableRow.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
 
-            // Creation textView that displays the all the flight airline.
-            TextView flightText = new TextView(this);
-            flightText.setText(flight.getAirline());
-            flightText.setLayoutParams(new TableRow.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
-            flightText.setPadding(5,5,5,5);
-            tableRow.addView(flightText);
-
-            // Creation of the textview that displays the number
+            // Creation of the textview that displays the Origin
             TextView flightText2 = new TextView(this);
-            flightText2.setText(Objects.toString(flight.getNumber()));;
+            flightText2.setText(flight.getOrigin());;
             flightText2.setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
             flightText2.setPadding(5,5,5,5);
             tableRow.addView(flightText2);
 
-            // Creation of the textView that displays the duration of the trip.
+            // Creation of the textView that displays the destination
             TextView flightText3 = new TextView(this);
-            flightText3.setText(Objects.toString(flight.getDuration()));
+            flightText3.setText(flight.getDestination());
             flightText3.setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
             tableRow.addView(flightText3);
@@ -70,21 +67,47 @@ public class ViewSearchedFlights extends AppCompatActivity {
             flightText4.setText(Objects.toString(flight.getPrice()));
             flightText4.setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
-            flightText4.setPadding(5,5,5,5);
+            flightText4.setPadding(5, 5, 5, 5);
             tableRow.addView(flightText4);
 
-            // Creation of the textView that displays the num available seats.
+            //Creation of the textView that displays the duration of the trip
             TextView flightText5 = new TextView(this);
-            flightText5.setText(Objects.toString(flight.getNumEmptySeats()));
+            flightText5.setText(Objects.toString(flight.getDuration()));
             flightText5.setLayoutParams(new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
-            flightText5.setPadding(5,5,5,5);
+            flightText5.setPadding(5, 5, 5, 5);
             tableRow.addView(flightText5);
+
+            //Adds a button to view the flights in the itinerary
+            viewButton view = new viewButton(this);
+            view.setText("View");
+            view.setTextSize(8);
+            view.flight = flight;
+            view.setOnClickListener(this);
+            tableRow.addView(view, 100, 100);
+
 
             //adds the row to the table
             table.addView(tableRow, new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
         }
     }
+
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(this, ViewSearchedFlights.class);
+        intent.putExtra("Flights", ((viewButton)view).flight.getFlights());
+        startActivity(intent);
+    }
+
+        //constructor for a custom button.
+        public class viewButton extends Button {
+            public Itinerary flight;
+
+            public viewButton(Context c) {
+                super(c);
+            }
+        }
+
 
 }
