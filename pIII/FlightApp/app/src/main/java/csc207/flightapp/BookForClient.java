b2ef3dd0a79943f820154f3ce6c83042c184b3d9
Sooham Rafiz <rafizsooham@gmail.com>
2015-12-08 13:10:34 +0000

    added 3 new admin menu activities, everything works perfectly

---
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/BookForClient.java
@@ -0,0 +1,14 @@
+package csc207.flightapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BookForClient extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_for_client);
+    }
+
+}
---
+++ pIII/FlightApp/app/src/main/java/csc207/flightapp/SearchForClients.java
@@ -0,0 +1,13 @@
+package csc207.flightapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchForClients extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_for_clients);
+    }
+}
